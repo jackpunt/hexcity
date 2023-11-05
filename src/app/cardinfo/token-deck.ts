be58a42580abeb3336732f2dfcbc13047426400f
jackpunt <jpeckj@gmail.com>
2023-11-04 19:35:01 -0700

    extras: +textLow, -vp,ext,etc

--- src/app/cardinfo/token-deck.ts
+++ src/app/cardinfo/token-deck.ts
@@ -5,15 +5,15 @@ export class TokenDeck {
    static deck: Deck = {
       name: 'TokenDeck',
       cards: [
-        {nreps: 32, type: "Debt", name: "Debt", color: "rgb(224,92,0)", cost: null, step: 0, stop: 0, rent: 0, vp: null, subtype: null, props: {noStop: true}, text: null, extras: [  ], path: "Debt.png"},
-        {nreps: 12, type: "house", name: "House", color: "rgb(4,201,0)", cost: 2, step: 0, stop: 0, rent: 0.5, vp: 1, subtype: null, props: {noStop: true}, text: null, extras: [ ], path: "House.png"},
-        {nreps: 12, type: "house", name: "Triplex", color: "rgb(4,201,0)", cost: 5, step: 0, stop: 0, rent: 1, vp: 3, subtype: null, props: {noStop: true}, text: null, extras: [  ], path: "Triplex.png"},
-        {nreps: 12, type: "house", name: "Apartments", color: "rgb(4,201,0)", cost: 8, step: 0, stop: 0, rent: 2, vp: 6, subtype: null, props: {noStop: true}, text: null, extras: [ ], path: "Apartments.png"},
-        {nreps: 12, type: "house", name: "High Rise", color: "rgb(4,201,0)", cost: 11, step: 0, stop: 0, rent: 4, vp: 10, subtype: null, props: {noStop: true}, text: null, extras: [ ], path: "High-Rise.png"},
-        {nreps: 12, type: "house", name: "Tower", color: "rgb(4,201,0)", cost: 14, step: 0, stop: 0, rent: 8, vp: 15, subtype: null, props: {noStop: true}, text: null, extras: [ ], path: "Tower.png"},
-        {nreps: 24, type: "marker", name: "VcOwned-RED", cost: 1, step: 0, stop: 0, rent: 0, text: null, color: "rgb(239,32,60)", extras: [  ], path: "VcOwned-RED.png"},
-        {nreps: 24, type: "marker", name: "VcOwned-BLUE", cost: 1, step: 0, stop: 0, rent: 0, text: null, color: "rgb(162,162,255)", extras: [  ], path: "VcOwned-BLUE.png"},
-        {nreps: 24, type: "marker", name: "VcOwned-GREEN", cost: 1, step: 0, stop: 0, rent: 0, text: null, color: "rgb(21,180,0)", extras: [  ], path: "VcOwned-GREEN.png"},
+        {nreps: 32, type: "Debt", name: "Debt", color: "rgb(224,92,0)", cost: null, step: null, stop: null, rent: 0, vp: null, subtype: null, props: {noStop: true}, text: null, extras: [  ], path: "Debt.png"},
+        {nreps: 12, type: "house", name: "House", color: "rgb(4,201,0)", cost: 2, step: null, stop: null, rent: 0.5, vp: 1, subtype: null, props: {noStop: true}, text: null, extras: [  ], path: "House.png"},
+        {nreps: 12, type: "house", name: "Triplex", color: "rgb(4,201,0)", cost: 5, step: null, stop: null, rent: 1, vp: 3, subtype: null, props: {noStop: true}, text: null, extras: [  ], path: "Triplex.png"},
+        {nreps: 12, type: "house", name: "Apartments", color: "rgb(4,201,0)", cost: 8, step: null, stop: null, rent: 2, vp: 6, subtype: null, props: {noStop: true}, text: null, extras: [  ], path: "Apartments.png"},
+        {nreps: 12, type: "house", name: "High Rise", color: "rgb(4,201,0)", cost: 11, step: null, stop: null, rent: 4, vp: 10, subtype: null, props: {noStop: true}, text: null, extras: [  ], path: "High-Rise.png"},
+        {nreps: 12, type: "house", name: "Tower", color: "rgb(4,201,0)", cost: 14, step: null, stop: null, rent: 8, vp: 15, subtype: null, props: {noStop: true}, text: null, extras: [  ], path: "Tower.png"},
+        {nreps: 24, type: "marker", name: "VcOwned-RED", cost: 1, step: null, stop: null, rent: 0, text: null, color: "rgb(239,32,60)", extras: [  ], path: "VcOwned-RED.png"},
+        {nreps: 24, type: "marker", name: "VcOwned-BLUE", cost: 1, step: null, stop: null, rent: 0, text: null, color: "rgb(162,162,255)", extras: [  ], path: "VcOwned-BLUE.png"},
+        {nreps: 24, type: "marker", name: "VcOwned-GREEN", cost: 1, step: null, stop: null, rent: 0, text: null, color: "rgb(21,180,0)", extras: [  ], path: "VcOwned-GREEN.png"},
         {nreps: 1, type: "Owner", name: "Owner-WHITE-0", cost: 0, color: "rgb(255,255,255)", path: "Owner-WHITE-0.png"},
    ]
   }}
